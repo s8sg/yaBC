3c2ec1b14a3b4d795a6c53af8d4bc72b4ffbcb2e
s8sg <swarvanusg@gmail.com>
2018-05-29 14:16:09 +0900

    add proof of work with nounce to calculate hash
    Signed-off-by: s8sg <swarvanusg@gmail.com>

--- main.go
+++ main.go
@@ -4,16 +4,19 @@ import (
 	"crypto/sha256"
 	"encoding/json"
 	"fmt"
+	"strings"
 )
 
 type Block struct {
 	Data     string
+	Nounce   string
 	Hash     string
 	PrevHash string
 }
 
 const (
 	genesisHash = "000000000000000000000000000000000000"
+	nounceRule  = "00000"
 )
 
 // A Store that maps Blocks against its hash
@@ -23,20 +26,54 @@ var blockStore = make(map[string]*Block)
 var blockChain string
 
 // A function to Generate hash
-func generateHash(newB *Block) string {
+func generateHash(newB *Block) (string, string) {
+	copyB := make([]string, 3)
+	nounce := 0
+	hash := ""
+	// generate a nounce and a hash that matches the nounce rule
+	for true {
+		// we use array to maintain the order
+		// [0] -> Data
+		// [1] -> Nounce
+		// [2] -> PrevHash
+		copyB[0] = newB.Data
+		copyB[1] = fmt.Sprintf("%d", nounce)
+		copyB[2] = newB.PrevHash
+		dump, _ := json.Marshal(copyB)
+		h := sha256.New()
+		h.Write(dump)
+		hash = fmt.Sprintf("%x", h.Sum(nil))
+		if strings.HasPrefix(hash, nounceRule) {
+			break
+		}
+		nounce++
+	}
+	return hash, fmt.Sprintf("%d", nounce)
+}
+
+// A function to Verify Hash
+func verifyHash(block *Block) (bool, string) {
 	// we use array to maintain the order
-	copyB := make([]string, 2)
-	copyB[0] = newB.Data
-	copyB[1] = newB.PrevHash
+	// [0] -> Data
+	// [1] -> Nounce
+	// [2] -> PrevHash
+	copyB := make([]string, 3)
+	copyB[0] = block.Data
+	copyB[1] = block.Nounce
+	copyB[2] = block.PrevHash
 	dump, _ := json.Marshal(copyB)
 	h := sha256.New()
 	h.Write(dump)
-	return fmt.Sprintf("%x", h.Sum(nil))
+	hash := fmt.Sprintf("%x", h.Sum(nil))
+	if block.Hash == hash {
+		return true, hash
+	}
+	return false, hash
 }
 
 // A function that adds a block to the blockchain
 func addToBlockChain(newB *Block) {
-	newB.Hash = generateHash(newB)
+	newB.Hash, newB.Nounce = generateHash(newB)
 	// add to blockStore
 	blockStore[newB.Hash] = newB
 	// change the last block hash
@@ -51,15 +88,21 @@ func verifyBlockChain(debug bool) bool {
 		if debug {
 			fmt.Printf("Block %s { \n Data: %s,\n PrevHash: %s \n}\n", block.Hash, block.Data, block.PrevHash)
 		}
-		hash := generateHash(block)
-		// Check if current hash is right
-		if hash != block.Hash {
-			fmt.Printf("Invalid block hash for block, expected hash %s, but found: %s\n", hash, block.Hash)
+		// Check if previousHash of the next block matchs
+		if previousHash != "" && previousHash != block.Hash {
+			fmt.Printf("Invalid block hash in next block, expected hash: %s, but found: %s\n", previousHash, block.Hash)
 			return false
 		}
-		// Check if previousHash of the next block matchs
-		if previousHash != "" && previousHash != hash {
-			fmt.Printf("Invalid block hash in next block, expected hash %s, but found: %s\n", previousHash, hash)
+
+		// Check if the hash match nounce value
+		if !strings.HasPrefix(block.Hash, nounceRule) {
+			fmt.Printf("Invalid hash as nounce doesn't match, block hash: %s, but nounce rule: %s\n", block.Hash, nounceRule)
+			return false
+		}
+
+		// Check if the block hash is valid
+		if verified, hash := verifyHash(block); !verified {
+			fmt.Printf("Invalid hash for block, block hash: %s, but generated hash: %s\n", block.Hash, hash)
 			return false
 		}
 
